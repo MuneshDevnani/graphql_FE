d0359f7a0d899a719b5dd689b03931d3085bc62a
MuneshDevnani <muneshdevnani@gmail.com>
2021-06-12 17:02:47 +0500

    first commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,48 @@
+import React, { useContext } from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+import { Grid, Image, Transition } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
+import { AuthContext } from "../context/auth";
+import PostForm from "../components/PostForm";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
+
+function Home() {
+  const { user } = useContext(AuthContext);
+  const { loading, data: { getPosts: posts } = {} } =
+    useQuery(FETCH_POSTS_QUERY);
+  return (
+    <Grid columns={3} divided style={{ justifyContent: "center" }}>
+    
+      <Grid.Row >
+        {user && (
+          <Grid.Column width={8}>
+            <PostForm />
+          </Grid.Column>
+        )}
+         </Grid.Row>
+         <Grid.Row>
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+         <Grid.Row>
+        {loading ? (
+          <h1>Loading posts...</h1>
+        ) : (
+          <Transition.Group>
+            {posts &&
+              posts.map((post) => (
+                <Grid.Row
+                  key={post.id}
+                  style={{ marginBottom: 20, marginLeft: 30 }}
+                >
+                  <PostCard post={post} />
+                </Grid.Row>
+              ))}
+          </Transition.Group>
+        )}
+      </Grid.Row>
+    </Grid>
+  );
+}
+
+export default Home;
---
+++ src/components/PostCard.js
@@ -0,0 +1,49 @@
+import moment from "moment";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Button, Card, Icon, Image, Label } from "semantic-ui-react";
+import { AuthContext } from "../context/auth";
+import LikeButton from "./LikeButton";
+import DeleteButton from "./DeleteButton";
+import MyPopup from "../util/MyPopup";
+
+function PostCard({
+  post: { id, body, createdAt, username, likeCount, commentCount, likes },
+}) {
+  const { user } = useContext(AuthContext);
+  const likePost = () => {};
+  return (
+    <Card>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow()}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <LikeButton user={user} post={{ id, likes, likeCount }} />
+        <MyPopup content="Comment on post">
+        <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
+          <Button color="blue" basic>
+            <Icon name="comments" />
+          </Button>
+          <Label basic color="blue" pointing="left">
+            {commentCount}
+          </Label>
+        </Button>
+        </MyPopup>
+        {user && user.username === username && (
+          <DeleteButton postId={id} />
+        )}
+      </Card.Content>
+    </Card>
+  );
+}
+
+export default PostCard;
